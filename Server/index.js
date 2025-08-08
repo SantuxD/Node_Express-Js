1a791d8cfa1b6293a50a44b32298fa82abd5e6d6
Santu Mondal <mondalsantu104@gmail.com>
2025-08-08 20:21:31 +0530

    Learn Restful api

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,69 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+const fs = require("fs");
+const data = require("./MOCK_DATA.json");
+const port = 8000;
 
-app.get("/", (req, res) => {
-    res.send("Hello from My Home page")
+fs.writeFile("./log.txt", "Basic log", () => {});
+
+app.get("/api/users", (req, res) => {
+  return res.json(data);
+});
+
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+       ${data
+         .map((user) => `<li>${user.first_name} ${user.last_name} </li>`)
+         .join("")}
+    
+    </ul>
+    
+    `;
+  res.send(html);
+});
+
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const user = data.find((user) => user.id === id)
+  return res.json(user);
+});
+
+
+app.post("/api/users",(req, res)=>{
+    return res.json({status: "pending"})
+
 })
 
-app.get("/about",(req,res)=>{
-    res.send(`This is my about page`)
-})
+
+// GET,PATCH, DELETE req for this path - "/api/users/:id"
+
+//  app
+//    .route("/api/users/:id")
+//    .get((req, res) =>{
+//          const id = Number(req.params.id)
+//     const user = data.find((user) => user.id === id)
+//   return res.json(user);
+//    })
+//    .patch((req,res)=>{
+//     return res.json({status:"pending"})
+//    })
+//    .delete((req,res)=>{
+//     return res.json({status:"pending"})
+//    })
 
 
-app.listen(8001, () => {
-    console.log(`Server Started:`);
-})
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server Started in: ${port}`);
+});
